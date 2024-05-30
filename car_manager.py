38aec09e372a11f83094a873fe1ba3e5820c1140
ksg-dev <ksg.dev.data@gmail.com>
2024-05-30 14:52:01 -0500

    car manager class

---
+++ car_manager.py
@@ -0,0 +1,35 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    def __init__(self):
+        self.all_cars = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def make_car(self):
+        # Add random chance to create car - like rolling dice
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            car = Turtle("square")
+            car.penup()
+            car.shapesize(stretch_len=2, stretch_wid=1)
+            car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250)
+            car.goto(300, random_y)
+            self.all_cars.append(car)
+
+    def move(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
+
+
+
+
